3e9e25e8e447bd6e420e91be031d9f19a55d4813
SydVic <fabrizio.bocca27@gmail.com>
2022-06-01 09:44:32 +0200

    aggiunto stile a form di contatto

--- src/components/MainContactUs.vue
+++ src/components/MainContactUs.vue
@@ -2,7 +2,7 @@
   <section class="contact-us">
     <div class="container-general">
       <div class="row">
-        <div class="col-lg-8">
+        <div class="col-lg-7">
           <div class="form-wrapper">
             <div class="form-text">
               <h3 class="title-small">send a message</h3>
@@ -11,20 +11,25 @@
                 <span class="title-highlighted txt-brand">Touch</span>
               </h2>
               <p class="paragraph">We will respond to your message as soon as possible.</p>
-              <form action="">
-                <input type="text" class="" placeholder="Name">
+              <!-- FORM -->
+              <form class="contact-us" action="">
+                <input type="text" class="input-name" placeholder="Name">
                 <input type="email" class="" placeholder="Email">
                 <input type="text" class="" placeholder="Phone">
-                <select name="" id="">
+                <select class="options-select" name="" id="">
                   <option value="">More Info</option>
                 </select>
-                <input type="text" class="" placeholder="Message">
+                <textarea class="leave-message" placeholder="Message"></textarea>
               </form>
+              <!-- /FORM -->
+              <div class="btn">
+                <div class="btn-primary">send</div>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4">
-          <div class="contacts-wrapper"></div>
+        <div class="col-lg-4 col-lg-offset-1">
+          <div class="contacts-wrapper">col 4</div>
         </div>
       </div>
     </div>
@@ -45,6 +50,55 @@ section.contact-us {
 
   .paragraph {
     color: $shark;
+  }
+
+  .contact-us {
+    width: 100%;
+    margin-top: 3.5rem;
+
+    input[type=text],
+    input[type=email],
+    select,
+    textarea {
+      padding: .9rem .7rem;
+      background-color: $gray-nurse;
+      border-color: transparent;
+      border-radius: 5px;
+      color: #8175a1;
+      font-size: 1rem;
+      margin-bottom: 1rem;
+
+      &::placeholder {
+        font-size: 1rem;
+        color: #8175a1;
+      }
+    }
+
+    input[type=text] {
+      width: calc(50% - .5rem);
+      margin-right: .5rem;
+    }
+
+    input[type=email] {
+      width: calc(50% - .5rem);
+      margin-left: .5rem;
+    }
+
+    select.options-select {
+      width: calc(50% - .5rem);
+      margin-left: .5rem;
+    }
+
+    textarea.leave-message {
+      font-family: "Lato", sans-serif;
+      width: 100%;
+      min-height: 150px;
+      resize: none;
+    }
+  }
+
+  .btn {
+    margin-top: .5rem;
   }
 }
 </style>
